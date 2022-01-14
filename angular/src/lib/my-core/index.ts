2c27deeeba8d0e7c7dc0bf1aaffddca8771f9496
Fernandopy <Fernandoabaran@gmail.com>
2022-01-14 13:37:46 +0100

    dinamico

--- angular/src/lib/my-core/index.ts
+++ angular/src/lib/my-core/index.ts
@@ -1,3 +1,3 @@
-export { MyCoreModule } from './modules/my-core.module'
+export { MyCoreModule } from './my-core.module'
 export * from './services/logger.service'
 export { ElipsisPipe, CapitalizePipe } from './pipes/cadenas.pipe'
